ac3aaf298aec0d664dbc0e6e58562ef88ddcdc06
Arun Raju <42838503+arunraju1701@users.noreply.github.com>
2022-01-23 18:17:28 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
 import { NavBarData } from './NavigationData'
 
